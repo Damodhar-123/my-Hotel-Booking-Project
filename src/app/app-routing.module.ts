f50fb1cb27b74dac5ca41e15338de6a6dd5a2071
Damodhar-123 <maskedamodhar@gmail.com>
2023-05-01 22:32:37 +0530

    routing edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminComponent } from './admin/admin.component';
-import { LoginsuccessComponent } from './admin/loginsuccess/loginsuccess.component';
 import { HomeComponent } from './home/home.component';
 import { HotelListComponent } from './hotel-list/hotel-list.component';
-import { NewHotelRegisterComponent } from './owner/new-hotel-register/new-hotel-register.component';
-import { OwnerComponent } from './owner/owner.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { UserComponent } from './user/user.component';
 import { SnackbarComponent } from './snackbar/snackbar.component';
 
 const routes: Routes = [ 
   {path: '', component:HomeComponent},
   {path: 'home', component:HomeComponent},
-  {path :'admin',component:AdminComponent},
-  {path :'owner',component:OwnerComponent},
-  {path: 'user', component:UserComponent},
   { path:'signup',component:SignUpComponent},
   {path : 'signin', component:SignInComponent},
-  {path:'loginsuccess',component:LoginsuccessComponent},
   {path:'hotel-list',component:HotelListComponent},
   {path: 'snackbar',component: SnackbarComponent},
 
